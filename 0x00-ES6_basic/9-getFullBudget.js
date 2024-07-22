57cad5d34521c5c793c2d81e2bdb268ca4b6e87c
Tunde <tundeogunmodede1@gmail.com>
2024-07-22 16:23:29 -0700

    fix

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from './7-getBudgetObject';
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
@@ -7,6 +7,6 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars: (income) => `$${income}`,
     getIncomeInEuros: (income) => `${income} euros`,
   };
- 
+
   return fullBudget;
 }
